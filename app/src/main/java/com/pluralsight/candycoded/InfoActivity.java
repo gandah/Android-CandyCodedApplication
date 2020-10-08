6951647eb4f411cca923252b7ef57b8dda281447
Blessing Nyuwani <blessingnyuwani@gmail.com>
2020-10-08 10:39:04 +0200

    Add Implicit and Explicit Intents

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -1,8 +1,10 @@
 package com.pluralsight.candycoded;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -23,11 +25,21 @@ public class InfoActivity extends AppCompatActivity {
 
     }
 
-    // ***
-    // TODO - Task 2 - Launch the Google Maps Activity
-    // ***
 
-    // ***
-    // TODO - Task 3 - Launch the Phone Activity
-    // ***
+
+    public void createMapIntent(View view){
+        Uri locationUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, locationUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+        if(mapIntent.resolveActivity(getPackageManager())!=null){
+            startActivity(mapIntent);
+        }
+    }
+
+    public void createPhoneIntent(View view){
+        Uri phoneUri = Uri.parse("tel:0123456789");
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL) ;
+        phoneIntent.setData(phoneUri);
+        startActivity(phoneIntent);
+    }
 }
